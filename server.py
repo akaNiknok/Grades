6bf0d2d8389460057a7009c23e14ad818079d330
akaNiknok <austingimperial@gmail.com>
2017-04-30 19:23:26 +0800

    User Database
    plus .gitignore

--- server.py
+++ server.py
@@ -3,6 +3,23 @@ from flask import redirect, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object("config")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    acc_type = db.Column(db.String(7), nullable=False)
+
+    def __init__(self, username, password, acc_type):
+        self.username = username
+        self.password = password
+        self.acc_type = acc_type
+
+
+db.create_all()
 
 
 @app.route('/')
