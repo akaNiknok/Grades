626d7d0a079eb283230c63f06a5d0c59c06cdc24
akaNiknok <austingimperial@gmail.com>
2017-05-19 20:27:23 +0800

    Update config and add multi-threading

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Flask-SQLAlchemy
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Flask
 SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = True
--- server.py
+++ server.py
@@ -241,5 +241,4 @@ def read_excels(grade, section, cn):
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", threaded=True)
