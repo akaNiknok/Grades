1f51971c6bd789255e1f8e9016d19db30b0d4c18
akaNiknok <austingimperial@gmail.com>
2017-06-20 21:30:48 +0800

    Read from the pre-rendered html

--- excels.py
+++ excels.py
@@ -1,14 +1,15 @@
 import os
 import openpyxl
 from openpyxl.utils import range_boundaries
+from bs4 import BeautifulSoup
 
 
-def read_excels(grade, section, cn):
-    """Read excels of every subject of a student and returns a table list
+def read_htmls(grade, section, cn):
+    """Read pre-rendered tables of every subject of a student and return a dict
     Return format:
         subjects = {
             "Subject":
-                "Trimester": [[col]]
+                "Trimester": pre_rendered table
         }"""
 
     # Get file directory using users grade and section
@@ -22,92 +23,33 @@ def read_excels(grade, section, cn):
 
     subjects = {}
 
-    # Loop per subject
+    # Loop through subjects
     for file in files:
 
-        # Only read .xslx
-        if file.endswith(".xlsx"):
-
-            # Open the excel file
-            wb = openpyxl.load_workbook(os.path.join(filedir, file),
-                                        data_only=True)
+        # only read .html.j2 files
+        if file.endswith(".j2"):
 
             trimesters = {}
 
-            # Loop through sheets
-            for ws in wb.worksheets:
+            # Open the pre-rendered html
+            with open("excels/{}/{}/{}".format(grade, section, file)) as f:
+                soup = BeautifulSoup(f.read().decode("utf-8"))
 
-                if "Raw.Score" in ws.title:
+            # Loop through trimesters
+            for div in soup.find_all("div"):
+                rows = div.find_all("tr")
 
-                    # Get boundary (min_col, min_row, max_col, max_row)
-                    # of merged_cells
-                    merged_cells = [range_boundaries(r)
-                                    for r in ws.merged_cell_ranges]
+                # Remove all rows except for headers and user's row
+                for row in rows:
+                    if rows.index(row) not in (0, 1, 2, 3, cn+3):
+                        row.decompose()
 
-                    table = []
+                # Store the div in the trimesters
+                trimesters[div.get("id")] = str(div)
 
-                    # Loop through rows
-                    for row in range(5, ws.max_row):
-
-                        # Store the labels
-                        if row in (5, 6, 7, 8):
-
-                            # Contains columns (value, colspan) here
-                            new_row = []
-
-                            # Create an iterator object to skip merged cells
-                            columns = iter(range(1, ws.max_column))
-
-                            # Loop through columns
-                            for column in columns:
-
-                                value = ws.cell(row=row, column=column).value
-
-                                # Check if the cell is not empty
-                                if value is not None:
-                                    colspan = 1
-
-                                    # Check if cell is merged
-                                    for r in merged_cells:
-                                        if (r[0] == column) and (r[1] == row):
-                                            # Add to colspan
-                                            colspan += (r[2] - r[0])
-                                            break
-
-                                    new_row.append((value, colspan))
-
-                                    # If cell is merged,
-                                    # skip (colspan - 1) iterations
-                                    if colspan != 1:
-                                        for x in range(colspan - 1):
-                                            next(columns)
-                                else:
-                                    new_row.append(("", 1))
-
-                            table.append(new_row)
-
-                        # Store users score
-                        elif ws.cell(row=row, column=1).value == cn:
-
-                            # Contains columns (value, colspan) here
-                            new_row = []
-
-                            # Loop through columns
-                            for column in range(1, ws.max_column):
-
-                                value = ws.cell(row=row, column=column).value
-
-                                # Check if the cell is not empty
-                                if value is not None:
-                                    new_row.append((value, 1))
-                                else:
-                                    new_row.append(("", 1))
-
-                            table.append(new_row)
-
-                    trimesters[ws.title.split("-")[1]] = table
-
-            subjects[os.path.splitext(file)[0]] = trimesters
+            # Store the trimester in the subjects
+            # And also get the filename as the key
+            subjects[file.split(".")[0]] = trimesters
 
     return subjects
 
